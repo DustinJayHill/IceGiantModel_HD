224cf05b0bf180b89f2a662c2d70d444e2f0b149
DustinJayHill <djh349@drexel.edu>
2017-04-27 13:43:04 -0400

    Changed units to cgs, added some constants

--- PLUTO/definitions.h
+++ PLUTO/definitions.h
@@ -19,10 +19,17 @@
 #define  ROTATING_FRAME          NO
 
 /* -- user-defined parameters (labels) -- */
-
+/*  these parameters are based on values for Neptune */
+#define  PLANET_RADIUS          2.4553e9
+#define  SURFACE_GRAVITY        1115.0
+#define  SOUND_SPEED            1.51908e5  /* Should be based on surface value */
 
 /* [Beg] user-defined constants (do not change this line) */
+/* Units are always in cgs */
 
+#define  UNIT_LENGTH            1.0
+#define  UNIT_VELOCITY          1.0
+#define  UNITY_DENSITY          1.0
 
 /* [End] user-defined constants (do not change this line) */
 
